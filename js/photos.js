0a4ce8dce0efff31c8b07b1d3725ea2d96e06a97
Ocelot-7 <thomas.eucat@protonmail.com>
2026-01-02 00:49:30 +0900

    Update photos.js

--- js/photos.js
+++ js/photos.js
@@ -35,6 +35,22 @@ const photos = {
     "633A2670_lifeof",
     "633A2647_da8bf9",
     "v1767280805/IMGL1205_ekhgm3.jpg",
+    "633A2876-2_ijjdyz",
+    "633A2291_kzpht8",
+    "633A2332_tieznu",
+    "633A2670_lifeof",
+    "633A2633_hsprzi",
+    "633A2947-3_ipnlh1",
+    "633A2948_dbigqw",
+    "633A2823_yi1gyn",
+    "633A2801-2_ehmumd",
+    "633A2791_nrly1v",
+    "633A2742_hbcanb",
+    "633A2678_gdmuxc",
+    "633A9782_snev9q",
+    "633A9750-2_v9yu1p",
+    "633A9737_htbmdo",
+    "633A9774_lncvuz",
 
    
   ],
@@ -66,6 +82,15 @@ const photos = {
    "IMGL1877-2_q2sw5t",
    "IMGL1874_ffzmwk",
    "IMGL1877-2_q2sw5t",
+     "633A4646_pxyvi9",
+    "633A4645_p2vyfe",
+    "633A4851_ucuix0",
+    "633A4773_koghen",
+    "IMGL6576-2_zbyfji",
+    "633A4052_cuq4jg",
+    "633A0337_lzgtym",
+    "633A0381-2_ojc6ke",
+    "633A0355-3_kmm3qa",
 
 
   
@@ -74,6 +99,28 @@ const photos = {
 
     blackwhite: [
     "v1767280805/IMGL1205_ekhgm3.jpg",
+    "IMGL0084_ttuta6",
+    "IMGL9617-7_qymeu0",
+    "IMGL7785_shac7l",
+    "IMGL6653_be7igj",
+    "IMGL6648_pvlcjd",
+    "IMGL6647_g29lkh",
+    "IMGL6508-3_ouclo0",
+    "633A4646_pxyvi9",
+    "633A4645_p2vyfe",
+    "633A4056_kxx1kr",
+    "633A2876-2_ijjdyz",
+    "633A2819-2_phpifn",
+    "633A9747_xc6jb3",
+    "633A9668_dkksrt",
+    "633A9668_dkksrt",
+    "633A8526_hhzmsl",
+    "633A8526_hhzmsl",
+    "633A8262-2_om5rzu",
+    "633A8255_fqbvfv",
+    "633A8154_ggv7bg",
+    "IMGL6671_slrzx3",
+    "IMGL6649_hhaf0m",
 
 
   
